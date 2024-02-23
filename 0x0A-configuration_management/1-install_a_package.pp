7cc4a04eb0534cc456ada0192258d7193ad9df15
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-02-23 22:21:52 +0200

    add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs flask from pip3 version 2.1.0.
-
-package { 'flask' :
-    ensure   => '2.1.0',
+# Installs flask from pip3
+package { 'flask':
+    ensure   => 2.1.0,
     provider => 'pip3',
 }
